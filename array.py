0ddea55a5a78c5cad89d3c649b2adf0037caa6a6
Rachit11-lang <148413315+Rachit11-lang@users.noreply.github.com>
2023-10-20 23:26:20 +0545

    Create array.py

---
+++ array.py
@@ -0,0 +1,8 @@
+# Create an unsorted array
+arr = [4, 2, 8, 1, 6, 3, 7]
+
+# Use the sorted() function to sort the array in ascending order
+sorted_arr = sorted(arr)
+
+# Print the sorted array
+print("Sorted array in ascending order:", sorted_arr)
